ad60ef4d43b5e1e23df2db7c3e62a7c1e6a3b648
Michael Osgood <michaelosgood@dhcp-v120-099.mobile.uci.edu>
2017-09-07 21:29:54 -0700

    connected to sql database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,39 @@
 var inquirer = require('inquirer');
 var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host:"localhost",
+    port: 3306,
+    user: "root",
+    password: "Boomers11!",
+    database: "bamazon_db"
+})
+
+connection.connect(function(err, answer){
+    if (err) throw err;
+    console.log("Connected as ID: " + connection.threadId);
+    var query = "SELECT item_id, product_name, price FROM products";
+    connection.query(query, { item_id: answer.item_id }, function(err, res) {
+        for (var i = 0; i < res.length; i++) {
+            console.log("Item ID: " + res[i].item_id + " || Product: " + res[i].product_name + " || Price: " + res[i].price);
+        }
+        shop();
+    }); 
+});
+
+function shop() {
+    inquirer.prompt({
+        name: "ID",
+        type: "input",
+        message: "What is the ID of the product you would like to purchase?",
+        validate: function(value) {
+            if (isNaN(value) === false) {
+              return true;
+            }
+            return false;
+          }
+    }).then(function(answer){
+        console.log("congratulations");
+    })
+}
+
